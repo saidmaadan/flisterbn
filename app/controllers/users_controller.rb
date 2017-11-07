f43d17f7a335add102ceee6cecf3b011dce9bc65
Said Maadan <me@saidmaadan.com>
2017-11-07 10:04:13 -0600

    Integrated Jquery Raty to customized Review stars

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, except: [:index, :new, :create]
   def show
-    @user = User.friendly.find(params[:id])
     @listings = @user.listings
 
     @guest_reviews = Review.where(type: "GuestReview", host_id: @user.id)
 
     @host_reviews = Review.where(type: "HostReview", guest_id: @user.id)
     #redirect_to root_path, notice: "You don't have permission to see this profile" unless current_user == @user
+  end
+
+  def set_user
+    @user = User.friendly.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,15 +17,14 @@ class User < ApplicationRecord
    def slug_users
      [
        :username,
-       [:username, :user_name],
-       [:username, :user_name, :fullname]
+       [:username, :user_name]
      ]
    end
 
 
-  # validates :first_name, presence: true, length:{maximum: 20}
-  # # validates :last_name, presence: true,  length:{maximum: 20}
-  # validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true, length:{maximum: 20}
+  validates :last_name, presence: true,  length:{maximum: 20}
+  #validates :username, presence: true, uniqueness: { case_sensitive: false }
 
   has_many :listings
   has_many :reservations
@@ -37,11 +36,7 @@ class User < ApplicationRecord
    end
 
    def user_name
-       if username
-         return username
-     else
-       [full_name, @user_id].join('')
-     end
+       [first_name, last_name, @user_id].join('')
    end
 
    def fullname
@@ -59,8 +54,8 @@ class User < ApplicationRecord
         user.password = Devise.friendly_token[0,20]
         user.first_name = auth.info.first_name
         user.last_name = auth.info.last_name
-        # user.last_name = auth.info.name
-        user.username = auth.extra.raw_info.username
+        user.username = auth.info.username
+        # user.username = auth.extra.raw_info.username
         user.image = auth.info.image
         user.uid = auth.uid
         user.provider = auth.provider
@@ -71,25 +66,5 @@ class User < ApplicationRecord
       end
     end
   end
-
-
-  #  def self.from_omniauth(auth)
-  #    user = User.where(email: auth.info.email).first
-  #    if user
-  #      return user
-  #    else
-  #      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #         user.email = auth.info.email
-  #         user.password = Devise.friendly_token[0,20]
-  #         user.full_name = auth.info.name
-  #         user.image = auth.info.image
-  #         user.username = auth.info.username
-  #         user.uid = auth.uid
-  #         user.provider = auth.provider
-  #         # If you are using confirmable and the provider(s) you use validate emails,
-  #         # uncomment the line below to skip the confirmation emails.
-  #         user.skip_confirmation!
-  #       end
-  #    end
-  # end
 end
+  # user.last_name = auth.info.name
