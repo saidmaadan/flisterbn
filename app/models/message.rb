a19aaa1f093da92c662f2a64381b40dd053a9b1c
Said Maadan <me@saidmaadan.com>
2017-11-15 00:43:13 -0600

    Implemented private messages exhange between users

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,23 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
+
+  validates_presence_of :context, :conversation_id, :user_id
+  after_create_commit :create_notification
+
+  def message_time
+    self.created_at.strftime("%v")
+  end
+
+  private
+
+    def create_notification
+      if self.conversation.sender_id == self.user_id
+        sender = User.find(self.conversation.sender_id)
+        Notification.create(context: "New message from #{sender.full_name}", user_id: self.conversation.recipent_id)
+      else
+        sender = User.find(self.conversation.recipient_id)
+        Notification.create(context: "New message from #{sender.full_name}", user_id: self.conversation.sender_id)
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,9 @@ Rails.application.routes.draw do
   end
 
   resources :revenues, only: [:index]
+  resources :conversations, only: [:index, :create] do
+    resources :messages, only: [:index, :create]
+  end
 
   get '/host_calendar' => "calendars#host"
   get '/payment_method' => 'users#payment'
